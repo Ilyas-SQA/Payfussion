2372df0bf40686170287f4c006c69846325c5533
Ilyas-SQA <ilyas.ahmad.mscs-s2022a@cecosian.edu.pk>
2025-10-21 00:13:34 +0500

    change color

--- lib/presentations/currency-exchange/calculator_view.dart
+++ lib/presentations/currency-exchange/calculator_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:payfussion/core/theme/theme.dart';
 import 'package:payfussion/presentations/currency-exchange/widget/calculator_widget.dart';
 import 'package:payfussion/presentations/currency-exchange/widget/convert_widget.dart';
 
@@ -16,37 +17,58 @@ class CalculatorView extends StatelessWidget {
       child: DefaultTabController(
         length: 2,
         child: Scaffold(
-          body: Column(
+          appBar: AppBar(
+            title: const Text("Calculator"),
+            centerTitle: true,
+            bottom: PreferredSize(
+              preferredSize: Size.fromHeight(60.h),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8).r,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF1E2A38),
+                    borderRadius: BorderRadius.circular(12.r),
+                  ),
+                  child: TabBar(
+                    dividerColor: Colors.transparent,
+                    padding: EdgeInsets.all(10.r),
+                    indicator: BoxDecoration(
+                      color: MyTheme.primaryColor,
+                      borderRadius: BorderRadius.circular(12.r),
+                    ),
+                    labelColor: Colors.white,
+                    unselectedLabelColor: Colors.grey,
+                    labelStyle: TextStyle(
+                      fontSize: 16.sp,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    unselectedLabelStyle: TextStyle(
+                      fontSize: 16.sp,
+                      fontWeight: FontWeight.normal,
+                    ),
+                    tabs: [
+                      const Tab(
+                        child: Padding(
+                          padding: EdgeInsets.all(10),
+                          child: Text('Calculator'),
+                        ),
+                      ),
+                      const Tab(
+                        child: Padding(
+                          padding: EdgeInsets.all(10),
+                          child: Text('Exchange Rate'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          body: const TabBarView(
             children: [
-              50.verticalSpace,
-              TabBar(
-                dividerColor: Colors.transparent,
-                indicatorWeight: 0.1,
-                indicatorColor: Colors.blue,
-                tabAlignment: TabAlignment.center,
-                unselectedLabelColor: Colors.grey, // Changed from Colors.white to Colors.grey
-                labelColor: Colors.blue,
-                labelStyle: TextStyle(
-                  fontSize: 18.sp,
-                  fontWeight: FontWeight.bold,
-                ),
-                unselectedLabelStyle: TextStyle(
-                  fontSize: 16.sp,
-                  fontWeight: FontWeight.normal,
-                ),
-                tabs: const <Widget>[
-                  Tab(text: 'Calculator'),
-                  Tab(text: 'Exchange Rate'),
-                ],
-              ),
-              const Expanded(
-                child: TabBarView(
-                  children: [
-                    CalculatorWidget(),
-                    ConvertWidget(),
-                  ],
-                ),
-              ),
+              CalculatorWidget(),
+              ConvertWidget(),
             ],
           ),
         ),
