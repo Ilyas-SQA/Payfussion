c6be5bb332edc5b913272becae5e70793f7ad896
Ilyas-SQA <ilyas.ahmad.mscs-s2022a@cecosian.edu.pk>
2025-10-28 23:53:54 +0500

    change ui some customization

--- lib/presentations/auth/sign_in/sign_in.dart
+++ lib/presentations/auth/sign_in/sign_in.dart
@@ -5,7 +5,6 @@ import 'package:payfussion/logic/blocs/auth/auth_bloc.dart';
 import 'package:payfussion/logic/blocs/auth/auth_state.dart';
 import 'package:payfussion/presentations/auth/sign_in/sign_in_form.dart';
 import 'package:payfussion/shared/widgets/error_dialog.dart';
-
 import '../../../core/constants/routes_name.dart';
 import '../../../services/biometric_service.dart';
 import '../../../services/service_locator.dart';
@@ -34,30 +33,28 @@ class _SignInScreenState extends State<SignInScreen> {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: BlocListener<AuthBloc, AuthState>(
-        listener: (context, state) {
+        listener: (BuildContext context, AuthState state) {
           if (state is AuthLoading) {
             showDialog(
               context: context,
               barrierDismissible: false,
               builder: (_) => const Center(child: CircularProgressIndicator()),
             );
-          } else if (state is SignInSuccess ||
-              state is BiometricAuthSuccess) {
+          } else if (state is SignInSuccess || state is BiometricAuthSuccess) {
             Navigator.of(context).pop();
             context.pushReplacement(RouteNames.homeScreen);
-          } else if (state is AuthStateFailure ||
-              state is BiometricAuthFailure) {
+          } else if (state is AuthStateFailure || state is BiometricAuthFailure) {
             Navigator.of(context).pop();
-            final message = state is AuthStateFailure
-                ? state.message
-                : (state as BiometricAuthFailure).message;
-
+            final String message = state is AuthStateFailure ? state.message : (state as BiometricAuthFailure).message;
             ErrorDialog.show(context, message);
           }
         },
         child: const Column(
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: [SignInHeader(), SignInForm()],
+          children: [
+            SignInHeader(),
+            SignInForm(),
+          ],
         ),
       ),
     );
