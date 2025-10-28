618f380e97cd34b57fe99349ab2cf93115da5e36
Ilyas-SQA <ilyas.ahmad.mscs-s2022a@cecosian.edu.pk>
2025-10-28 23:55:10 +0500

    add validation

--- lib/core/widget/text_field/phone_textfield.dart
+++ lib/core/widget/text_field/phone_textfield.dart
@@ -1,18 +1,23 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:intl_phone_field/intl_phone_field.dart';
+import 'package:intl_phone_field/phone_number.dart';
 import '../../theme/theme.dart';
 
 class PhoneCredentialsField extends StatelessWidget {
   final TextEditingController controller;
   final String helpText;
   final Function(String)? onPhoneChanged;
+  final FutureOr<String?> Function(PhoneNumber?)? validator;
 
   const PhoneCredentialsField({
     Key? key,
     required this.controller,
     required this.helpText,
     this.onPhoneChanged,
+    this.validator,
   }) : super(key: key);
 
   @override
@@ -20,57 +25,30 @@ class PhoneCredentialsField extends StatelessWidget {
     return IntlPhoneField(
       controller: controller,
       cursorColor: MyTheme.primaryColor,
-      textAlignVertical: TextAlignVertical.center,
-      cursorHeight: 14,
+      cursorHeight: 18,
+      validator: validator,
       decoration: InputDecoration(
         hintText: helpText,
-        contentPadding: EdgeInsets.only(bottom: 24.h, left: 15.w),
-        /// Normal Border
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.all(Radius.circular(8.r)),
-          borderSide: BorderSide(
-            color: Colors.grey.shade300,
-            width: 1,
-          ),
-        ),
-        /// Focused Border
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.all(Radius.circular(8.r)),
-          borderSide: const BorderSide(
-            color: MyTheme.primaryColor,
-            width: 1.5,
-          ),
-        ),
-        /// Error Border
-        errorBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.all(Radius.circular(8.r)),
-          borderSide: const BorderSide(
-            color: Colors.red,
-            width: 1.2,
-          ),
-        ),
-        /// Focused Error Border
-        focusedErrorBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.all(Radius.circular(8.r)),
-          borderSide: const BorderSide(
-            color: Colors.red,
-            width: 1.5,
-          ),
-        ),
         hintStyle: TextStyle(
-            fontSize: 14.sp,
-            fontWeight: FontWeight.normal
+          fontSize: 14.sp,
+          fontWeight: FontWeight.normal,
+        ),
+        contentPadding: EdgeInsets.symmetric(
+          vertical: 16.h,
+          horizontal: 19.w,
         ),
       ),
       style: TextStyle(
-          fontSize: 14.sp,
-          fontWeight: FontWeight.normal
+        fontSize: 14.sp,
+        fontFamily: 'Inter',
+        fontWeight: FontWeight.w500,
       ),
-      initialCountryCode: 'PK', // Pakistan
+      initialCountryCode: 'PK',
       dropdownIconPosition: IconPosition.leading,
       showCountryFlag: true,
       showDropdownIcon: false,
-      onChanged: (phone) {
+      flagsButtonPadding: const EdgeInsets.symmetric(horizontal: 10),
+      onChanged: (PhoneNumber phone) {
         if (onPhoneChanged != null) {
           onPhoneChanged!(phone.completeNumber);
         }
