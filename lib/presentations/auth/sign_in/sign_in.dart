f191e09a0c74d2e5ce74c83ae49f4b18803bfb45
Ilyas-SQA <ilyas.ahmad.mscs-s2022a@cecosian.edu.pk>
2025-10-31 02:35:11 +0500

    add theme in background

--- lib/presentations/auth/sign_in/sign_in.dart
+++ lib/presentations/auth/sign_in/sign_in.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
@@ -8,9 +6,9 @@ import 'package:payfussion/logic/blocs/auth/auth_state.dart';
 import 'package:payfussion/presentations/auth/sign_in/sign_in_form.dart';
 import 'package:payfussion/shared/widgets/error_dialog.dart';
 import '../../../core/constants/routes_name.dart';
-import '../../../core/theme/theme.dart';
 import '../../../services/biometric_service.dart';
 import '../../../services/service_locator.dart';
+import '../../widgets/background_theme.dart';
 import 'sign_in_header.dart';
 
 class SignInScreen extends StatefulWidget {
@@ -38,13 +36,14 @@ class _SignInScreenState extends State<SignInScreen> with TickerProviderStateMix
 
   @override
   void dispose() {
-    // TODO: implement dispose
-    super.dispose();
     _backgroundAnimationController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    final bool isDarkMode = Theme.of(context).brightness == Brightness.dark;
+
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: BlocListener<AuthBloc, AuthState>(
@@ -66,46 +65,11 @@ class _SignInScreenState extends State<SignInScreen> with TickerProviderStateMix
         },
         child: Stack(
           children: [
-            AnimatedBuilder(
-              animation: _backgroundAnimationController,
-              builder: (BuildContext context, Widget? child) {
-                return Stack(
-                  children: List.generate(8, (int index) {
-                    /// Calculate movement path for each circle
-                    final double angle = (_backgroundAnimationController.value * 2 * pi) + (index * pi / 4);
-                    final double radiusX = 150 + (index * 20);
-                    final double radiusY = 200 + (index * 30);
-
-                    final double left = MediaQuery.of(context).size.width / 2 + cos(angle) * radiusX - 125;
-                    final double top = MediaQuery.of(context).size.height / 2 + sin(angle) * radiusY - 125;
-
-                    /// Different sizes and colors
-                    final double size = 150 + (index * 30) + sin(_backgroundAnimationController.value * 2 * pi) * 20;
-                    final Color circleColor = index % 3 == 0 ? MyTheme.primaryColor.withOpacity(0.15) : index % 3 == 1 ? MyTheme.secondaryColor.withOpacity(0.15) : MyTheme.secondaryColor.withOpacity(0.15);
-
-                    return Positioned(
-                      left: left,
-                      top: top,
-                      child: Container(
-                        width: size,
-                        height: size,
-                        decoration: BoxDecoration(
-                          color: circleColor,
-                          shape: BoxShape.circle,
-                          boxShadow: [
-                            BoxShadow(
-                              color: circleColor.withOpacity(0.3),
-                              blurRadius: 40,
-                              spreadRadius: 10,
-                            ),
-                          ],
-                        ),
-                      ),
-                    );
-                  }),
-                );
-              },
+            // Animated Background
+            AnimatedBackground(
+              animationController: _backgroundAnimationController,
             ),
+            // Content
             const Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
